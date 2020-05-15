b8424110f8a38de46abe11c74131adac97bbf539
Robert Haines <rhaines@manchester.ac.uk>
2020-05-15 10:45:10 +0100

    Test `RegionEstimatorFactory.add_factory`.
    This needs a change to how we test the `RegionEstimatorFactory.factories`
    member as well - so combine the two tests.

--- test/test_factory.py
+++ test/test_factory.py
@@ -7,12 +7,6 @@ class TestFactory(unittest.TestCase):
   Tests for RegionEstimatorFactory.
   """
 
-  def test_no_factories_at_start(self):
-    """
-    Test that RegionEstimatorFactory.factories starts off empty.
-    """
-    self.assertEqual(RegionEstimatorFactory.factories, {}, 'factories should be initialized empty')
-
   def test_get_classname(self):
     """
     Test that the get_classname method works as expected.
@@ -21,6 +15,19 @@ class TestFactory(unittest.TestCase):
     self.assertEqual(get_classname('distance-simple'), 'DistanceSimpleEstimator')
     with self.assertRaises(ValueError):
       get_classname('___')
+
+  def test_add_factory(self):
+    """
+    Test that add_factory works as expected.
+    """
+    self.assertEqual(RegionEstimatorFactory.factories, {}, 'factories should be initialized empty')
+
+    RegionEstimatorFactory.add_factory('id', 'something')
+    self.assertEqual(RegionEstimatorFactory.factories['id'], 'something')
+    self.assertEqual(RegionEstimatorFactory.factories, {'id': 'something'})
+
+    RegionEstimatorFactory.add_factory('id', 'else')
+    self.assertEqual(RegionEstimatorFactory.factories['id'], 'else')
 
 if __name__ == '__main__':
   unittest.main()
