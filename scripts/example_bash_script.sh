93935256ab6c52ace0b0d8ff9718dd84670a05fe
annannfryingpan <ann.gledson@manchester.ac.uk>
2021-02-07 11:05:58 +0000

    Improved shell script comments and added a 4th example

--- scripts/example_bash_script.sh
+++ scripts/example_bash_script.sh
@@ -12,19 +12,29 @@ MAX_PROCESSORS=3
 
 
 # EXAMPLE 1
+# Specifi request for single timestamp / region
 ARGUMENTS_1="--outdir ${OUTDIR} --outfile_suffix ${OUTFILE_SUFFIX} --timestamp ${TIMESTAMP} --region_id ${REGION_ID} \
 --method ${METHOD} --measurement ${MEASUREMENT} --verbose ${VERBOSE} --max_processors ${MAX_PROCESSORS}"
 python region_estimation_script.py ${ARGUMENTS_1}
 
 # EXAMPLE 2
+# All regions timestamp request
 VERBOSE=0
 ARGUMENTS_2="-o ${OUTDIR} -u ${OUTFILE_SUFFIX} -t ${TIMESTAMP} -m ${METHOD} -e ${MEASUREMENT} -v ${VERBOSE} -p ${MAX_PROCESSORS}"
 
 python region_estimation_script.py ${ARGUMENTS_2}
 
 # EXAMPLE 3
+# Timeseries style request (no timestamp)
 VERBOSE=0
 REGION_ID='SK'
 ARGUMENTS_3="-o ${OUTDIR} -u ${OUTFILE_SUFFIX} -g ${REGION_ID} -m ${METHOD} -e ${MEASUREMENT} -v ${VERBOSE} -p ${MAX_PROCESSORS}"
 
 python region_estimation_script.py ${ARGUMENTS_3}
+
+# EXAMPLE 4
+# Get estmates for all timestamps / regions (can be slow if we have a large dataset)
+VERBOSE=0
+ARGUMENTS_4="-o ${OUTDIR} -u ${OUTFILE_SUFFIX}  -m ${METHOD} -e ${MEASUREMENT} -v ${VERBOSE} -p ${MAX_PROCESSORS}"
+
+python region_estimation_script.py ${ARGUMENTS_4}
